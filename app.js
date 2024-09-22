e44909f83a85d07e772488ee1f8abfc989d937be
Muhannad1798 <Muhannad987m@gmail.com>
2024-09-22 18:13:59 +0300

    HomeWork1-1

---
+++ app.js
@@ -0,0 +1,245 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x
+  } else {
+    return y
+  }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9))
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+const isAdult = (x) => {
+  if (x > 17) {
+    return 'adult'
+  } else {
+    return 'minor'
+  }
+}
+console.log('Exercise 2 Result:', isAdult(7))
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+const isCharAVowel = (y) => {
+  if (y === 'a' || y === 'e' || y === 'o' || y === 'u' || y === 'i') {
+    return true
+  } else return false
+}
+console.log('Exercise 3 Result:', isCharAVowel('a'))
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+const generateEmail = (x, y) => {
+  return `${x}@${y}`
+}
+console.log('Exercise 4 Result:', generateEmail('johnsmith', 'example.com'))
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+const greetUser = (x, y) => {
+  if (y === 'morning') {
+    return `Good ${y}, ${x}!`
+  } else if (y === 'afternoon') {
+    return `Good ${y}, ${x}!`
+  } else if (y === 'evening') {
+    return `Good ${y}, ${x}!`
+  } else return 'pleas enter a valid time like (morning, afternoon, evening)'
+}
+console.log('Exercise 5 Result:', greetUser('Sam', 'morning'))
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+const maxOfThree = (x, y, z) => {
+  if (x > y) {
+    if (x > z) {
+      return x
+    } else return z
+  } else {
+    if (y > z) return y
+    else return z
+  }
+}
+console.log('Exercise 6 Result:', maxOfThree(17, 10, 20))
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+const calculateTip = (x, y) => {
+  return (y / 100) * x
+}
+console.log('Exercise 7 Result:', calculateTip(10, 20))
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+const convertTemperature = (x, y) => {
+  if (y === 'C') {
+    let f = x * (9 / 5) + 32
+    return `${f} (Fahrenheit)`
+  } else if (y === 'F') {
+    let c = (x - 32) * (5 / 9)
+    return `${c} (Celsius)`
+  } else return 'please enter a valid string like (C,F)'
+}
+console.log('Exercise 8 Result:', convertTemperature(32, 'F'))
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+const basicCalculator = (x, y, z) => {
+  if (z === 'add') return x + y
+  else if (z === 'subtract') return x - y
+  else if (z === 'multiply') return x * y
+  else if (z === 'divide') return x / y
+  else
+    "please write a valid operation like ('add', 'subtract', 'multiply', 'divide') "
+}
+console.log('Exercise 9 Result:', basicCalculator(10, 5, 'subtract'))
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+const calculateGrade = (x) => {
+  if (x > 89 && x < 101) return 'A'
+  else if (x > 79) return 'B'
+  else if (x > 69) return 'C'
+  else if (x > 59) return 'D'
+  else if (x >= 0) return 'F'
+  else return ' please write a valid number between 0 and 100'
+}
+console.log('Exercise 10 Result:', calculateGrade(0))
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+const createUsername = (x, y) => {
+  let a = x.substring(0, 3)
+  let b = y.substring(0, 3)
+  let c = x.length + y.length
+  return `${a}${b}${c}`
+}
+console.log('Exercise 11 Result:', createUsername('amantha', 'Green'))
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+const numArgs = (x) => {
+  return x.length
+}
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4))
